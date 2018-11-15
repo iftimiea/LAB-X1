f038f216845011788fa1fbfa6f4cf8ad2bb1e464
iftimiea <37336935+iftimiea@users.noreply.github.com>
2018-11-14 21:42:01 -0500

    Empty project

---
+++ lab-pic18-platform/PIC18F.X/main.c
@@ -0,0 +1,40 @@
+/**********************************************************************
+* File name: main.c
+* 
+* Descriptions:
+*
+*
+*
+* Revision History:
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* Author            Date      		Comments on this revision
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* your name            
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*
+* Additional notes:
+*
+**********************************************************************/
+
+#include <xc.h>
+#include <delays.h>
+#include "../common/lcd.h"
+#include "../common/initBoard.h"
+
+#define MS50 10
+#define MS100 20
+#define S1 200
+
+
+int main(void) {
+    initOscillator();
+    initIO();
+    LCDInit();
+    LCDClear();
+    
+    while (1) {
+    
+        
+	} //end of while
+}// end of main
+
